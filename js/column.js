03425046928cb902ac996aa415d17b8a88d20417
Diego Enriquez-Serrano <diegoenriquez-serrano@Diegos-MacBook-Air.local>
2019-11-12 10:43:48 -0600

    forgot this useless file

---
+++ js/column.js
@@ -0,0 +1,7 @@
+const columns = Array.from(_d.getElementsByClassName("column"));
+
+const columnContainer = _d.getElementById("columnGrid");
+for( i = 0 ; i < columns.length ; i++ ) {
+ //console.log(columns[i].offsetTop); 
+ //console.log(columns[i].offsetHeight);
+}
