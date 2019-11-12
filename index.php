b38feac6c79124d1173ce4f0ca403de97d36bf1a
Diego Enriquez-Serrano <diegoenriquez-serrano@Diegos-MacBook-Air.local>
2019-11-12 13:22:00 -0600

    nothing big

--- index.php
+++ index.php
@@ -1,43 +1,26 @@
 <?php 
 
-    include("functions.php");
+include("functions.php");
 
-    include("views/header.html");
+include("views/header.html");
 
-    if(isset($_GET['page'])){
-
-        if ($_GET['page'] == 'home') {
-
-            include("views/home.html");
-
-        } else if ($_GET['page'] == 'drumming') {
-
-            include("views/drumming.html");
-
-        } else if ($_GET['page'] == 'column') {
-
-            include("views/column.html");
-
-        } else if ($_GET['page'] == 'art') {
-
-            include("views/art.html");
-
-        } else if ($_GET['page'] == 'photos') {
-
-            include("views/photos.html"); 
-
-        } else if ($_GET['page'] == 'search') {
-
-            include("views/search.php");
-
-        }
-
-    }else {
-
-        include("views/home.html");
-
-    }
-
-    include("views/footer.html");
+if(isset($_GET['page'])){
+  if ($_GET['page'] == 'home') {
+    include("views/home.html");
+  } else if ($_GET['page'] == 'drumming') {
+    include("views/drumming.html");
+  } else if ($_GET['page'] == 'column') {
+    include("views/column.html");
+  } else if ($_GET['page'] == 'art') {
+    include("views/art.html");
+  } else if ($_GET['page'] == 'photos') {
+    include("views/photos.html"); 
+  } else if ($_GET['page'] == 'search') {
+    include("views/search.php");
+  }
+} else {
+  include("views/home.html");
+}
+include("views/footer.html");
 
 ?>
